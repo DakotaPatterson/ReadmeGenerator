183b95873cc4f6efe20955d11502e6669aad7453
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-04-02 14:00:53 -0400

    Added the question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,65 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What would you like the description to be for this project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the installtion instructions for this project?',
+        name: 'installtion',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information for this project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What is the contribution guidelines for this project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What is the test instructions for this project?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'What license would you like to include in this project?',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'gitHub',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+            return;
+        }
+        console.log(`${fileName} has been saved.`);
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
